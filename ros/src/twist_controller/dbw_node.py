7a9137388fc6b6b96044b314b332a7d570a76f5d
Zaim Musa <musazaim@gmail.com>
2018-01-13 18:43:28 +0100

    update

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -53,9 +53,9 @@ class DBWNode(object):
         self.previous_loop_time = rospy.get_rostime()
         self.previous_debug_time = rospy.get_rostime()
 
-        self.throttle_pid = pid.PID(kp=1.0, ki=0.1, kd=0.0, mn=decel_limit, mx=0.5 * accel_limit)
-        self.brake_pid = pid.PID(kp=100.0, ki=0.0, kd=1.0, mn=brake_deadband, mx=5000)
-        self.steering_pid = pid.PID(kp=1.0, ki=0.0, kd=1.0, mn=-max_steer_angle, mx=max_steer_angle)
+        self.throttle_pid = pid.PID(kp=10.0, ki=0.01, kd=1.0, mn=decel_limit, mx=0.5 * accel_limit)
+        self.brake_pid = pid.PID(kp=120.0, ki=0.0, kd=1.0, mn=brake_deadband, mx=5000)
+        self.steering_pid = pid.PID(kp=1.0, ki=0.1, kd=0.6, mn=-max_steer_angle, mx=max_steer_angle)
 
 
         self.steer_pub = rospy.Publisher('/vehicle/steering_cmd',
