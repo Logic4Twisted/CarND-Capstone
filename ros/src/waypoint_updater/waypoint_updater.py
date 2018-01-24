838615044862a9282469cb66b3f6f78055234832
ZaimM <musazaim@gmail.com>
2018-01-24 20:26:01 +0100

    update

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -90,7 +90,7 @@ class WaypointUpdater(object):
 
     def traffic_cb(self, msg):
         self.traffic_light_wp_idx = msg.data
-		# pass
+        # pass
 
     def obstacle_cb(self, msg):
         # TODO: Callback for /obstacle_waypoint message. We will implement it later
